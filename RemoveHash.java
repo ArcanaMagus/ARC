5ac3868e1ab554e6131d6e5f5d0112ffdb7dbf22
ArcWare <ron.thomas2@student.tdsb.on.ca>
2015-06-06 07:34:59 -0400

    ARC::RemoveHash
    Whereas it my be relatively expensive to call the contains method, it
    should be used sporadically. In order to remove any value from the
    Hashtable, we first find the appropriate slot for this value and remove
    any association. In that place, we mark this as reserved to maintain
    consistency of locate.

---
+++ RemoveHash.java
@@ -0,0 +1,14 @@
+public Object remove(Object key)
+// pre: key is non-null Object
+// post: removes key-value pair associated with key
+{
+	int hash = locate(key);
+	Association z = data[hash];
+	if ( z == null || z == reserved) {
+		return null;
+	}
+	count--;
+	Object oldValue = a.value();
+	data[hash] = reserved; // in case anyone depends on u
+	return oldValue;
+}
