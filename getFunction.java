1ca60f1d80c1481a480f1a0db78a61660c43a7ce
ArcWare <ron.thomas2@student.tdsb.on.ca>
2015-06-06 07:16:26 -0400

    Revert "ARC::getFunction"
    This reverts commit a87fff98c8841d84e8375696f7f8e1f0dfd83e55.

--- getFunction.java
+++
@@ -1,9 +0,0 @@
-public Object get(Object key)
-// pre: key is non-null Object
-// post: returns value associated with key, or non-null
-{
-	int hash = locate(key);
-	Association z = data[hash];
-	if ( z == null || z == reserved) return null;
-	return data[hash].value();
-}
